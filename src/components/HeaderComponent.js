3c0db364538883d0c32a91f5efe056aeb943fc04
Eric George <eric@madebymany.co.uk>
2017-06-22 09:19:20 +0100

    sass

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -11,22 +11,22 @@ import NavigationClose from "material-ui/svg-icons/navigation/close";
 import NavigationRefresh from "material-ui/svg-icons/navigation/refresh";
 import CircularProgress from "material-ui/CircularProgress";
 
-import "../sass/Header.css";
+import "../sass/Menu.css";
 
 type Props = {
   fetchData: Function,
-  loading: Boolean,
+  loading: boolean,
 };
 
 type State = {
-  drawer: Boolean,
+  drawer: boolean,
 };
 
 export default class HeaderComponent extends React.PureComponent {
   props: Props;
   state: State;
 
-  handleToggle = () => this.setState({drawer: !this.state.drawer});
+  handleToggle = (): void => this.setState({drawer: !this.state.drawer});
 
   constructor (props: Props) {
     super(props);
@@ -54,14 +54,18 @@ export default class HeaderComponent extends React.PureComponent {
           iconElementLeft={<IconButton>{drawer ? <NavigationClose /> : <NavigationMenu />}</IconButton>}
           iconElementRight={this.reloadButton()}
           onLeftIconButtonTouchTap={this.handleToggle}
-        >
-        </AppBar>
-        <Drawer open={drawer} openSecondary>
+        />
+        <Drawer open={drawer}>
+          <AppBar
+            title="Menu"
+            iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+            onLeftIconButtonTouchTap={this.handleToggle}
+          />
           <MenuItem>
-            <Link to="/">Dashboard</Link>
+            <Link className="MenuItem__Link" to="/">Dashboard</Link>
           </MenuItem>
           <MenuItem>
-            <Link to="/chart">Chart</Link>
+            <Link className="MenuItem__Link" to="/chart">Chart</Link>
           </MenuItem>
         </Drawer>
       </div>
